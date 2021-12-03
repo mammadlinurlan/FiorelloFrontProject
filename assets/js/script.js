f6c8e28a2e11f876be70e1592dd380effa498312
mammadlinurlan <nurlanym@code.edu.az>
2021-12-04 00:06:40 +0400

    2,3. sections added. i am beast ;)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+
+    $(".flowersInfo").css("opacity","100")
+    
+
     $(".menu-icon i").click(function () {
         $(".menu").addClass("active")
 
@@ -7,23 +11,74 @@ $(document).ready(function () {
         $(".menu").removeClass("active")
     })
 
-    $(".menu .menu-nav-links li").click(function (e) {
+    $("")
+
+    let image = document.getElementById('interval')
+
+    let images = ['https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background.jpg','https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background-3.jpg','https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background-2.jpg']
+
+    $(image).css("opacity","100")
+  
+
+
+
+    setInterval(function(){
+        let random = Math.floor(Math.random()*3);
+        image.src = images[random]
+        // $(image).css('transform','scale(2)');
+        
+    }, 3000);
+
+    $(".clickright").click(function(){
+        let random = Math.floor(Math.random()*3);
+        image.src = images[random]
+    })
+    $(".clickleft").click(function(){
+        let random = Math.floor(Math.random()*3);
+        image.src = images[random]
+    })
+   
+    $(".menu .menu-nav-links li.1").click(function (e) {
         e.preventDefault();
         let sublink = $(this).children(".sublinks")
         
         let icon = $(this).find("i");
-       
+        console.log("aue");
+
         $(icon).toggleClass("customAnimate")
         $(sublink).slideToggle()
     })
 
     $(".menu .menu-nav-links li .sublinks li").click(function (e) {
+
         e.preventDefault()
-        let subsublink = $(this).children(".subSublinks")
-        console.log(this);
-        $(this).click(function(){
-            $(subsublink).slideToggle()
+        let subsublink = $(this).children(".sub-subLinks")
+        $(subsublink).slideToggle()
+
+        $(".menu .menu-nav-links li.1").children(".sublinks").slideUp()
+
+    }) 
+
+    let filters = document.querySelectorAll(".filters span")
+    let products = document.querySelectorAll(".product")
+    filters.forEach((filter)=>{
+        $(filter).click(function(){
+            let dataid = $(this).attr("data-id")
+            console.log(dataid);
+            products.forEach((delprod)=>{
+                $(delprod).addClass("unactiveProd")
+            })
+            let stayedprod = Array.from(document.getElementsByClassName(dataid))
+            stayedprod.forEach((prod)=>{
+                $(prod).removeClass("unactiveProd")
+            })
+          
+            })
             
         })
     })
-})
+
+
+
+
+   

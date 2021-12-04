460ae2a969c8d8290b76256c9561adbb446de39f
mammadlinurlan <nurlanym@code.edu.az>
2021-12-04 05:45:20 +0400

    2 sections left only

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
-    $(".flowersInfo").css("opacity","100")
-    
+    $(".flowersInfo").css("opacity", "100")
+
 
     $(".menu-icon i").click(function () {
         $(".menu").addClass("active")
@@ -15,33 +15,33 @@ $(document).ready(function () {
 
     let image = document.getElementById('interval')
 
-    let images = ['https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background.jpg','https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background-3.jpg','https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background-2.jpg']
+    let images = ['https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background.jpg', 'https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background-3.jpg', 'https://fiorello.qodeinteractive.com/wp-content/uploads/2018/05/h3-slider-background-2.jpg']
 
-    $(image).css("opacity","100")
-  
-
+    $(image).css("opacity", "100")
 
 
-    setInterval(function(){
-        let random = Math.floor(Math.random()*3);
+
+
+    setInterval(function () {
+        let random = Math.floor(Math.random() * 3);
         image.src = images[random]
         // $(image).css('transform','scale(2)');
-        
+
     }, 3000);
 
-    $(".clickright").click(function(){
-        let random = Math.floor(Math.random()*3);
+    $(".clickright").click(function () {
+        let random = Math.floor(Math.random() * 3);
         image.src = images[random]
     })
-    $(".clickleft").click(function(){
-        let random = Math.floor(Math.random()*3);
+    $(".clickleft").click(function () {
+        let random = Math.floor(Math.random() * 3);
         image.src = images[random]
     })
-   
+
     $(".menu .menu-nav-links li.1").click(function (e) {
         e.preventDefault();
         let sublink = $(this).children(".sublinks")
-        
+
         let icon = $(this).find("i");
         console.log("aue");
 
@@ -57,28 +57,81 @@ $(document).ready(function () {
 
         $(".menu .menu-nav-links li.1").children(".sublinks").slideUp()
 
-    }) 
-
-    let filters = document.querySelectorAll(".filters span")
-    let products = document.querySelectorAll(".product")
-    filters.forEach((filter)=>{
-        $(filter).click(function(){
-            let dataid = $(this).attr("data-id")
-            console.log(dataid);
-            products.forEach((delprod)=>{
-                $(delprod).addClass("unactiveProd")
-            })
-            let stayedprod = Array.from(document.getElementsByClassName(dataid))
-            stayedprod.forEach((prod)=>{
-                $(prod).removeClass("unactiveProd")
-            })
-          
-            })
-            
-        })
     })
 
 
 
+    let filters = document.querySelectorAll(".filters span")
+    let mdfilters = document.querySelectorAll(".mdfilters .mdcategories span")
 
-   
+
+    let products = document.querySelectorAll(".product")
+    $("#firstfilter").addClass("activeFilter")
+    filters.forEach((filter) => {
+        $(filter).click(function () {
+
+
+            filters.forEach((f) =>{
+                $(f).removeClass("activeFilter")
+            })
+
+
+            let dataid = $(this).attr("data-id")
+            console.log(dataid);
+            products.forEach((delprod) => {
+                $(delprod).addClass("unactiveProd")
+            })
+            let stayedprod = Array.from(document.getElementsByClassName(dataid))
+            stayedprod.forEach((prod) => {
+                $(prod).removeClass("unactiveProd")
+            })
+
+
+            $(this).addClass("activeFilter")
+        })
+
+
+    })
+
+    $("#mdcategory").click(function(){
+        $(".mdcategories").slideToggle();
+    })
+
+    $("#mdfilterbtn").click(function(){
+        $(".mdfilterLists").slideToggle();
+    })
+
+    mdfilters.forEach((filter) => {
+        $(filter).click(function () {
+
+
+            mdfilters.forEach((f) =>{
+                $(f).removeClass("activeFilter")
+            })
+
+
+            let dataid = $(this).attr("data-id")
+            console.log(dataid);
+            products.forEach((delprod) => {
+                $(delprod).addClass("unactiveProd")
+            })
+            let stayedprod = Array.from(document.getElementsByClassName(dataid))
+            stayedprod.forEach((prod) => {
+                $(prod).removeClass("unactiveProd")
+            })
+
+
+            $(this).addClass("activeFilter")
+        })
+
+
+    })
+
+
+    $('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
+})
+
+
+
+
+
